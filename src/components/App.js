ec9fb9c6c1032549dfd7e96b9a92ba7b3178bc71
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-20 20:19:55 +0100

    useEffect and fetch combo for dog data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [displayed, setDisplayed] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+      .then((r) => r.json())
+      .then((data) => {
+        // console.log(data);
+        setDisplayed(data.message);
+      });
+  }, []);
+
+  if (!displayed) return <p>Loading...</p>;
+
+  return (
+    <>
+      {<img src={displayed} alt="A Random Dog" width="500px" height="500px" />}
+    </>
+  );
+}
+
+export default App;
